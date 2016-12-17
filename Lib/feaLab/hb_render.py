22cbd7c406c8240a8f616dccdf54f587142d3a25
Adam Twardoch <adam@twardoch.com>
2016-12-17 11:05:02 +0100

    refactor

--- Lib/feaLab/hb_render.py
+++ Lib/feaLab/hb_render.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
+"""hb_render.py
+
 hb_render.HarfBuzzRenderer class 
 
 wraps the 'hb-view' and 'hb-shape' Harfbuzz utilities
@@ -49,69 +50,74 @@ docs tbd, this is from hb-view docs:
 
             """
 
-import sys
 import json
+import sys
 import warnings
-try: 
-        from sh import hb_shape
-except ImportError:
-        warnings.warn("Run: brew install harfbuzz")
-        warnings.warn("Run: pip install --user sh")
-try: 
-        from sh import hb_view
-        HB_VIEW = True
-except ImportError:
-        HB_VIEW = False
 
-class HarfBuzzRenderer(object): 
-    def __init__(self, font_file = None, face_index = 0, text = u''): 
+try:
+    from sh import hb_shape
+except ImportError:
+    warnings.warn("Run: brew install harfbuzz")
+    warnings.warn("Run: pip install --user sh")
+try:
+    from sh import hb_view
+
+    HB_VIEW = True
+    """
+    """
+except ImportError:
+    HB_VIEW = False
+
+
+class HarfBuzzRenderer(object):
+    def __init__(self, font_file=None, face_index=0, text=u''):
         self.shaper = 'ot'
         self.all_shapers = [self.shaper]
         self.shaper_os = self.shaper
 
-        self.shapers = [self.shaper]             # Set comma_separated list of shapers to try
+        self.shapers = [self.shaper]  # Set comma_separated list of shapers to try
 
-        self.font_file = font_file               # Set font file_name
-        self.face_index = face_index             # Set face index (default: 0)
-        self.font_size='upem'                    # Font size (default: upem)
+        self.font_file = font_file  # Set font file_name
+        self.face_index = face_index  # Set face index (default: 0)
+        self.font_size = 'upem'  # Font size (default: upem)
 
-        self.text=text                           # Set input text as Unicode
-        self.text_file=None                      # path to text file (text file overrides text)
+        self.text = text  # Set input text as Unicode
+        self.text_file = None  # path to text file (text file overrides text)
 
-        self.direction='auto'                    # Set text direction: 'ltr'|'rtl'|'ttb'|'btt'|'auto'
-        self.language='en'                       # Set text language ISO
-        self.script='auto'                       # Set text script (ISO_15924 tag, default: auto)
-        self.features=[]                         # list of font features e.g. ["aalt[3:5]=2","+kern","-liga"]
+        self.direction = 'auto'  # Set text direction: 'ltr'|'rtl'|'ttb'|'btt'|'auto'
+        self.language = 'en'  # Set text language ISO
+        self.script = 'auto'  # Set text script (ISO_15924 tag, default: auto)
+        self.features = []  # list of font features e.g. ["aalt[3:5]=2","+kern","-liga"]
 
-        self.bot=False                           # Treat text as beginning_of_paragraph
-        self.eot=False                           # Treat text as end_of_paragraph
-        self.text_before=u''                     # Set text context before each line
-        self.text_after=u''                      # Set text context after each line
+        self.bot = False  # Treat text as beginning_of_paragraph
+        self.eot = False  # Treat text as end_of_paragraph
+        self.text_before = u''  # Set text context before each line
+        self.text_after = u''  # Set text context after each line
 
-        self.preserve_default_ignorables=False   # Preserve Default_Ignorable characters
-        self.utf8_clusters=False                 # Use UTF8 byte indices, not char indices
-        self.cluster_level=0                     # Cluster merging level 0 | 1 | 2 (default: 0)
-        self.normalize_glyphs=False              # Rearrange glyph clusters in nominal order
-        self.num_iterations=1                    # Run shaper N times (default: 1)
-        self.use_glyph_indexes=False             # Output glyph indices instead of names
+        self.preserve_default_ignorables = False  # Preserve Default_Ignorable characters
+        self.utf8_clusters = False  # Use UTF8 byte indices, not char indices
+        self.cluster_level = 0  # Cluster merging level 0 | 1 | 2 (default: 0)
+        self.normalize_glyphs = False  # Rearrange glyph clusters in nominal order
+        self.num_iterations = 1  # Run shaper N times (default: 1)
+        self.use_glyph_indexes = False  # Output glyph indices instead of names
 
-        self.annotate=False                      # Annotate output toing
-        self.background='#ffffff'                # Set background color (rrggbb/rrggbbaa default: #FFFFFF)
-        self.foreground='#000000'                # Set foreground color (rrggbb/rrggbbaa, default: #000000)
-        self.line_space=0                        # Set space between lines in units (default: 0)
-        self.margin=[16, 16, 16, 16]             # Margin around output (one to four numbers , default: 16)
+        self.annotate = False  # Annotate output toing
+        self.background = '#ffffff'  # Set background color (rrggbb/rrggbbaa default: #FFFFFF)
+        self.foreground = '#000000'  # Set foreground color (rrggbb/rrggbbaa, default: #000000)
+        self.line_space = 0  # Set space between lines in units (default: 0)
+        self.margin = [16, 16, 16, 16]  # Margin around output (one to four numbers , default: 16)
 
-    def updateShapers(self): 
+    def updateShapers(self):
         self.all_shapers = str(hb_shape(list_shapers=True)).splitlines()
         if sys.platform.startswith('win32'):
             if 'directwrite' in self.all_shapers and 'uniscribe' not in self.all_shapers:
                 self.shaper_os = 'directwrite'
             elif 'uniscribe' in self.all_shapers:
                 self.shaper_os = 'uniscribe'
-        elif sys.platform.startswith('darwin'):    
+        elif sys.platform.startswith('darwin'):
             if 'coretext' in self.all_shapers:
                 self.shaper_os = 'coretext'
-        if 'ot' not in self.all_shapers: 
+        if 'ot' not in self.all_shapers:
             self.shaper = self.shaper_os
 
     def loadFont(self, font_file, face_index=0):
@@ -125,18 +131,18 @@ class HarfBuzzRenderer(object):
         hb_out = hb_shape(
             _in=hb_in,
             output_format='json',
-            font_file=self.font_file, 
-            face_index=self.face_index, 
-            font_size=self.font_size, 
+            font_file=self.font_file,
+            face_index=self.face_index,
+            font_size=self.font_size,
             show_text=False,
             show_unicode=False,
             show_extents=False,
             language=self.language,
-            direction=self.direction, 
-            script=self.script, 
-            features=",".join(self.features), 
-            bot=self.bot, 
-            eot=self.eot, 
+            direction=self.direction,
+            script=self.script,
+            features=",".join(self.features),
+            bot=self.bot,
+            eot=self.eot,
             text_before=self.text_before,
             text_after=self.text_after,
             preserve_default_ignorables=self.preserve_default_ignorables,
@@ -149,12 +155,12 @@ class HarfBuzzRenderer(object):
         )
         return json.loads(
             unicode(hb_out)
-            )
+        )
 
-    def toImage(self, text=None, format='svg', font_size=None, output_file=False): 
+    def toImage(self, text=None, format='svg', font_size=None, output_file=False):
         if not HB_VIEW:
             return None
-        else:  
+        else:
             font_size = font_size if font_size else self.font_size
             self.font_size = font_size
             text = text if text else self.text
@@ -163,19 +169,19 @@ class HarfBuzzRenderer(object):
             hb_out = hb_view(
                 _in=hb_in,
                 output_format=format,
-                output_file=output_file, 
-                font_file=self.font_file, 
-                face_index=self.face_index, 
-                font_size=font_size, 
+                output_file=output_file,
+                font_file=self.font_file,
+                face_index=self.face_index,
+                font_size=font_size,
                 show_text=False,
                 show_unicode=False,
                 show_extents=False,
                 language=self.language,
-                direction=self.direction, 
-                script=self.script, 
-                features=",".join(self.features), 
-                bot=self.bot, 
-                eot=self.eot, 
+                direction=self.direction,
+                script=self.script,
+                features=",".join(self.features),
+                bot=self.bot,
+                eot=self.eot,
                 text_before=self.text_before,
                 text_after=self.text_after,
                 preserve_default_ignorables=self.preserve_default_ignorables,
@@ -191,19 +197,20 @@ class HarfBuzzRenderer(object):
                 line_space=self.line_space,
                 margin=self.margin if type(self.margin) == int else " ".join(self.margin),
             )
-            if output_file: 
+            if output_file:
                 return output_file
-            else: 
+            else:
                 return hb_out
 
-    def toSVG(self, text=None, font_size=None, output_file=False): 
+    def toSVG(self, text=None, font_size=None, output_file=False):
         return self.toImage(text=text, font_size=font_size, output_file=output_file, format='svg')
 
-    def toPNG(self, text=None, font_size=None, output_file=False): 
+    def toPNG(self, text=None, font_size=None, output_file=False):
         return self.toImage(text=text, font_size=font_size, output_file=output_file, format='png')
 
-    def toPDF(self, text=None, font_size=None, output_file=False): 
+    def toPDF(self, text=None, font_size=None, output_file=False):
         return self.toImage(text=text, font_size=font_size, output_file=output_file, format='pdf')
+
 
 if __name__ == '__main__':
     hb = HarfBuzzRenderer()
@@ -211,7 +218,7 @@ if __name__ == '__main__':
     print(hb.all_shapers)
     hb.font_file = 'test/MinionPro-Regular.otf'
     print(hb.toJson(text='Hello'))
-    hb.margin=0
+    hb.margin = 0
     print(hb.toSVG(text='Hello', font_size=23))
     print(hb.toPNG(text='Hello', font_size=23, output_file='test.png'))
     print(hb.toPDF(text='Hello', font_size=23, output_file='test.pdf'))
